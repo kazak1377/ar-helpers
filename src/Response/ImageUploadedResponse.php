e36858d8642768f0bdec24d50af239c712057757
kazak <kazak1377@gmail.com>
2019-06-25 17:51:14 +0300

    New response type

---
+++ src/Response/ImageUploadedResponse.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: kazak
+ * Email: mk@altrecipe.com
+ * Date: 2019-04-29
+ * Time: 17:24
+ */
+
+namespace ArHelpers\Response;
+
+class ImageUploadedResponse extends BaseResponse {
+	public $code = 200;
+	public $message = "Image successfully uploaded";
+}

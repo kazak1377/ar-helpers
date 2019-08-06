0bcb442cfe44846afe98582607665e1358a25f2c
kazak <kazak1377@gmail.com>
2019-08-06 11:56:53 +0300

    Better unhandled error

--- src/Errors/UnhandledExceptionError.php
+++ src/Errors/UnhandledExceptionError.php
@@ -10,14 +10,20 @@ namespace ArHelpers\Errors;
 
 
 use ArHelpers\Error\BaseError;
+use Throwable;
 
 class UnhandledExceptionError extends BaseError {
     public $httpCode = '500';
     public $code = '65';
     public $message = 'Unhandled exception';
 
-    public function __construct($e) {
-        $this->description = $e;
+    public function __construct(Throwable $e) {
+        $this->description = [];
+        $this->description[] = "Message -- ".$e->getMessage();
+        $this->description[] = "File -- ".$e->getFile();
+        $this->description[] = "Line -- ".$e->getLine();
+        $this->description[] = "Code -- ".$e->getCode();
+        $this->description[] = "Trace -- ".$e->getTraceAsString();
         parent::__construct();
     }
 }

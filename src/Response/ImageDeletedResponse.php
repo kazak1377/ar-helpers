7ca555a6e1f712a71d845385645c4273dbf940f8
kazak <kazak1377@gmail.com>
2019-08-21 16:20:31 +0300

    New response added

---
+++ src/Response/ImageDeletedResponse.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: kazak
+ * Email: mk@altrecipe.com
+ * Date: 2019-04-29
+ * Time: 17:24
+ */
+
+namespace ArHelpers\Response;
+
+class ImageDeletedResponse extends BaseResponse {
+	public $code = 200;
+	public $message = "Image successfully deleted";
+}

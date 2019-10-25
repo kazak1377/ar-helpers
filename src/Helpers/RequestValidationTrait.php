b3a9f392c12b9ad277e36a339e2a1eae0be01d7c
Maxim Kozachenko <mk@altrecipe.com>
2019-10-25 20:49:05 +0300

    Added request validation trait

---
+++ src/Helpers/RequestValidationTrait.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace ArHelpers\Helpers;
+
+
+use ArHelpers\Errors\ValidationError;
+use ArHelpers\Response\BaseResponse;
+use Illuminate\Support\Facades\Request;
+
+trait RequestValidationTrait {
+    /**
+     * @param string $eName entity name
+     * @param array $rParams required params
+     * @param string $rClass response class
+     * @return BaseResponse
+     */
+    protected function getRequestValidationError($eName, $rParams, $rClass) {
+        $error = new ValidationError($eName);
+        $desc = [
+            "message" => "All params are required",
+            "required_params" => $rParams,
+            "your_request" => Request::input()
+        ];
+        $error->setDescription($desc);
+        /** @var BaseResponse $resp */
+        $resp = new $rClass();
+        return $resp
+            ->setError($error);
+    }
+
+    protected function requestIsValid($requiredFields) {
+        foreach ($requiredFields as $param) {
+            if (empty(Request::input($param))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
